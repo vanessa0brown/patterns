d49c10bdd7d7c29c660de95c005209154c4e4d0f
FIP <vanessa0brown@example.com>
2024-12-17 16:48:44 +0300

    update: pipeline with channels

--- pipeline/main.go
+++ pipeline/main.go
@@ -2,18 +2,76 @@ package main
 
 import "fmt"
 
+func add(doneCh chan struct{}, inputCh chan int) chan int {
+	resultChan := make(chan int)
+
+	go func() {
+		defer close(resultChan)
+
+		for value := range inputCh {
+			result := value + 2
+
+			select {
+			case <-doneCh:
+				return
+			case resultChan <- result:
+			}
+		}
+	}()
+
+	return resultChan
+}
+
+func multiply(doneCh chan struct{}, inputCh chan int) chan int {
+	resultChan := make(chan int)
+
+	go func() {
+		defer close(resultChan)
+
+		for value := range inputCh {
+			result := value * 3
+
+			select {
+			case <-doneCh:
+				return
+			case resultChan <- result:
+			}
+		}
+	}()
+
+	return resultChan
+}
+
+func generator(doneCh chan struct{}, numbers []int) chan int {
+	outputCh := make(chan int)
+
+	go func() {
+		defer close(outputCh)
+
+		for _, num := range numbers {
+			select {
+			case <-doneCh:
+				return
+			case outputCh <- num:
+			}
+		}
+	}()
+
+	return outputCh
+}
+
 func main() {
-	value := 1
+	numbers := []int{1, 2, 3, 4, 5}
 
-	fmt.Println(add(multiply(value, 2), 3))
+	doneCh := make(chan struct{})
+	defer close(doneCh)
 
-	fmt.Println(multiply(add(value, 2), 3))
-}
+	inputCh := generator(doneCh, numbers)
 
-func add(a, b int) int {
-	return a + b
-}
+	addCh := add(doneCh, inputCh)
+	resultCh := multiply(doneCh, addCh)
 
-func multiply(a, b int) int {
-	return a * b
+	for res := range resultCh {
+		fmt.Println(res)
+	}
 }
